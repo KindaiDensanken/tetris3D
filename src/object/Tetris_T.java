61d3b1b5c38241c951085458dc91b293adcb84d4
kindaidensan <densan.kindai@gmail.com>
2017-09-14 14:01:18 +0900

    Tetris_T

--- src/object/Tetris_T.java
+++ src/object/Tetris_T.java
@@ -1,11 +1,30 @@
 package object;
-
+/**
+*T型のテトリス
+*
+ */
 public class Tetris_T implements Tetris{
+
+
+
+	public class Tetris_T(PointOf3D controllPoint){
+
+		super(controllPoint);
+
+		PointList.add(new PointOf3D(0,0,0));
+		PointList.add(new PointOf3D(0,-1,0));
+		PointList.add(new PointOf3D(-1,-1,0));
+		PointList.add(new PointOf3D(1,-1,0));
+		if(checkMap()){
+			
+		}
+		assignMap();
+	}
+
 
 	@Override
 	public int getType() {
 		// TODO Auto-generated method stub
-		return 2;
-	}
+		return TetrisList.T.getTetrisNumber();
 
 }
